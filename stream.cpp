a7dc5514e5b9b85c3a5448dc83f94f3ad859c059
unreaIuser <93293947+unreaIuser@users.noreply.github.com>
2023-07-22 23:20:42 +0200

    fix mem leaks

--- stream.cpp
+++ stream.cpp
@@ -1,29 +1,28 @@
 #include "stream.hpp"
 
-
 bool utils::ReadFileToMemory(const std::string& file_path, std::vector<uint8_t>* out_buffer)
 {
-	std::ifstream file_ifstream(file_path, std::ios::binary);
+    std::ifstream file_ifstream(file_path, std::ios::binary);
 
-	if (!file_ifstream)
-		return false;
+    if (!file_ifstream)
+        return false;
 
-	out_buffer->assign((std::istreambuf_iterator<char>(file_ifstream)), std::istreambuf_iterator<char>());
-	file_ifstream.close();
+    out_buffer->assign((std::istreambuf_iterator<uint8_t>(file_ifstream)), std::istreambuf_iterator<uint8_t>());
+    file_ifstream.close();
 
-	return true;
+    return true;
 }
 
-bool utils::CreateFileFromMemory(const std::string& desired_file_path, const char* address, size_t size)
+bool utils::CreateFileFromMemory(const std::string& desired_file_path, const uint8_t* address, size_t size)
 {
-	std::ofstream file_ofstream(desired_file_path.c_str(), std::ios_base::out | std::ios_base::binary);
+    std::ofstream file_ofstream(desired_file_path.c_str(), std::ios_base::out | std::ios_base::binary);
 
-	if (!file_ofstream.write(address, size))
-	{
-		file_ofstream.close();
-		return false;
-	}
+    if (!file_ofstream.write(reinterpret_cast<const char*>(address), size))
+    {
+        file_ofstream.close();
+        return false;
+    }
 
-	file_ofstream.close();
-	return true;
+    file_ofstream.close();
+    return true;
 }
